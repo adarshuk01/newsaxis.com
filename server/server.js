b23c6eb7195b75ab57a09472f380a48292d9b7f3
Adarsh uk <adarshuk79@gmail.com>
2024-12-19 13:22:32 +0530

    changes in puppeter

--- server/server.js
+++ server/server.js
@@ -1,34 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-const newsRoutes = require('./routes/newsRoutes');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const app = express();
 
-// Configure CORS
+// Allow requests from the frontend
 const corsOptions = {
-  origin: ['https://newsaxis.vercel.app', 'http://localhost:3000'], // Allowed origins
+  origin: 'https://newsaxis.vercel.app', // Frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-  credentials: true, // Allow cookies if needed
 };
 
-// Apply CORS middleware
 app.use(cors(corsOptions));
 
-// Body parser
 app.use(bodyParser.json());
-
-// News routes
 app.use('/api/news', newsRoutes);
 
-// Define the port
-const PORT = process.env.PORT || 5002;
-
-// Start the server
-app.listen(PORT, '0.0.0.0', () => {
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
